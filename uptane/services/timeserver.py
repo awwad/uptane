f733f3053f6bdce88f2cb46e34669d75f64f47d3
Sebastien Awwad <sebastien.awwad@gmail.com>
2017-02-27 13:06:45 -0500

    have Timeserver ignore BER requests for now

--- uptane/services/timeserver.py
+++ uptane/services/timeserver.py
@@ -20,7 +20,9 @@ try:
 except ImportError:
  print('Minor: pyasn1 library not found. Proceeding using JSON only.')
 else:
- import uptane.ber_encoder as ber_encoder
+ #import uptane.ber_encoder as ber_encoder
+ # TODO: Add the modules necessary to handle timeserver attestation ASN1
+ # conversion to Uptane.
  PYASN1_EXISTS = True
 
 import time
@@ -86,13 +88,14 @@ def get_signed_time_ber(nonces):
 
   signed_dict = signable_time_attestation_as_dict['signed']
   dict_signatures = signable_time_attestation_as_dict['signatures']
-  asn_signed, ber_signed = timeserver.get_asn_and_ber_signed(timeservermetadata.get_asn_signed, signed_dict)
-  ber_metadata = timeserver.json_to_ber_metadata(asn_signed, ber_signed, dict_signatures)
+  #asn_signed, ber_signed = timeserver.get_asn_and_ber_signed(timeservermetadata.get_asn_signed, signed_dict)
+  #ber_metadata = timeserver.json_to_ber_metadata(asn_signed, ber_signed, dict_signatures)
   # To decode on other side:
   # dict_again = timeserver.ber_to_json_metadata(timeservermetadata.get_json_signed, ber_metadata)
 
   #signable_time_attestation_in_ber = \
   #    time_to_ber.get_asn_signed(signable_time_attestation_as_dict)
 
-  return ber_metadata
+#  return ber_metadata
 
+  raise NotImplementedError('Current code must be reconciled with new encodings for Timeserver.')

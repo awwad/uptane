687c2bbf573baecdbb7157b9c5f236be7fbe3bfa
Sebastien Awwad <sebastien.awwad@gmail.com>
2017-03-15 10:48:37 -0400

    Add live testing to demo Timeserver
    Before the demo Timeserver starts, it will test itself, ensuring that
     - Requests for normal (non-ASN.1, human readable) time attestations
       get reasonable responses.
     - DER-encoded ASN.1-converted signed time attestations can be
       generated, re-signed over the DER encoding, and validated.
    
    While this does not test the XML-RPC calls, it does test the Binary
    object encapsulation used for them. A full demo integration test
    should probably add that check at some point.

--- demo/demo_timeserver.py
+++ demo/demo_timeserver.py
@@ -4,7 +4,7 @@
 
 <Purpose>
   Acts as an Uptane-compliant Timeserver:
-   -Listens for requests from vehicles.
+   -Listens for requests from vehicles over XML-RPC.
    -Receives a list of nonces and responds with a signed time attestation
     that lists those nonces.
 
@@ -28,6 +28,10 @@ import tuf.formats
 from six.moves import xmlrpc_server
 from six.moves import xmlrpc_client # for Binary data encapsulation
 import uptane.services.timeserver as timeserver
+
+# These two imports are used solely for testing relevant to DER encoding.
+import uptane.encoding.asn1_codec as asn1_codec
+import hashlib
 
 
 # Restrict director requests to a particular path.
@@ -80,11 +84,17 @@ def listen(use_new_keys=False):
   timeserver.set_timeserver_key(load_timeserver_key(use_new_keys))
   print('Timeserver signing key loaded.')
 
+
+  # Test locally before opening the XMLRPC port.
+  test_demo_timeserver()
+
+
   # Create server
   server = xmlrpc_server.SimpleXMLRPCServer(
       (demo.TIMESERVER_HOST, demo.TIMESERVER_PORT),
       requestHandler=RequestHandler)#, allow_none=True)
   #server.register_introspection_functions()
+
 
   # Add a function to the Timeserver's xml-rpc interface.
   # Register function that can be called via XML-RPC, allowing a Primary to
@@ -93,12 +103,51 @@ def listen(use_new_keys=False):
   server.register_function(
       get_signed_time_der_wrapper, 'get_signed_time_der')
 
-  # Test locally
-  print(timeserver.get_signed_time([1, 2]))
-  print(timeserver.get_signed_time_der([2]))
 
   print('Timeserver will now listen on port ' + str(demo.TIMESERVER_PORT))
   server.serve_forever()
+
+
+
+
+
+def test_demo_timeserver():
+  """
+  Test the demo timeserver.
+  # TODO: Consider moving these tests into a demo integration test module.
+  """
+
+  # Fetch a normal signed time attestation, without ASN.1 format or DER
+  # encoding.
+  timeserver.get_signed_time([1, 2])
+
+
+  # Fetch a DER-encoded converted-to-ASN.1 signed time attestation, with a
+  # signature over the DER encoding.
+  der_signed_time = timeserver.get_signed_time_der([2, 9, 151])
+
+  # Encapsulate that in a Binary object for XML-RPC.
+  xb_der_signed_time = xmlrpc_client.Binary(der_signed_time)
+  assert der_signed_time == xb_der_signed_time.data, \
+      'Demo Timeserver self-test failed: xmlrpc Binary encapsulation issue'
+
+
+  # Validate that signature.
+  timeserver_key_pub = demo.import_public_key('timeserver')
+  tuf.formats.ANYKEY_SCHEMA.check_match(timeserver_key_pub)
+
+
+  for pydict_again in [
+      asn1_codec.convert_signed_der_to_dersigned_json(der_signed_time),
+      asn1_codec.convert_signed_der_to_dersigned_json(xb_der_signed_time.data)]:
+    der_signed = asn1_codec.convert_signed_metadata_to_der(
+          pydict_again, only_signed=True)
+    der_signed_hash = hashlib.sha256(der_signed).hexdigest()
+    assert tuf.keys.verify_signature(
+        timeserver_key_pub, pydict_again['signatures'][0],
+        der_signed_hash), \
+        'Demo Timeserver self-test failed: unable to verify signature over DER.'
+
 
 
 

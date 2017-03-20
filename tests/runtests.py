8d3c325959f0a436571ea3633bbfda6b4f04dd5e
Sebastien Awwad <sebastien.awwad@gmail.com>
2017-03-20 14:58:53 -0400

    Try fixing CI issue finding tests
    by specifying the start directory in tests/runtests.py as
    ./tests.

--- tests/runtests.py
+++ tests/runtests.py
@@ -16,6 +16,6 @@
 from unittest import defaultTestLoader, TextTestRunner
 import sys
 
-suite = defaultTestLoader.discover(start_dir=".")
+suite = defaultTestLoader.discover(start_dir="tests")
 result = TextTestRunner(verbosity=2).run(suite)
 sys.exit(0 if result.wasSuccessful() else 1)

0c2791eb077a419cfec89a20f6d63f48ea1dff71
Sebastien Awwad <sebastien.awwad@gmail.com>
2017-03-30 14:28:28 -0400

    DER: Correct and expand vehicle manifest conversion to DER

--- uptane/encoding/vehicle_manifest_asn1_coder.py
+++ uptane/encoding/vehicle_manifest_asn1_coder.py
@@ -38,10 +38,24 @@ def get_asn_signed(json_signed):
 
   for ecu_serial in json_signed['ecu_version_manifests']:
     for manifest in json_signed['ecu_version_manifests'][ecu_serial]:
+      temp_ecu_manifest = ECUVersionManifest()
       json_signed = manifest['signed']
       json_signatures = manifest['signatures']
-      asn_signed = ecu_manifest_asn1_coder.get_asn_signed(manifest)
-      ecuVersionManifest = asn_signed
+      # This is the 'signed' element of the ECU Manifest,
+      # and we need the full signable (including 'signatures').
+      ecu_manifest_signed = ecu_manifest_asn1_coder.get_asn_signed(
+          manifest['signed'])
+      # TODO: <~> RESOLVE CIRCULAR IMPORT and move.
+      # Will probably collapse the *coder.py modules into this module and
+      # refactor.
+      import uptane.encoding.asn1_codec as asn1_codec
+      ecu_manifest_signatures = asn1_codec.convert_signatures_to_asn(
+          manifest['signatures'])
+
+      temp_ecu_manifest['numberOfSignatures'] = len(ecu_manifest_signatures)
+      temp_ecu_manifest['signatures'] = ecu_manifest_signatures
+      temp_ecu_manifest['signed'] = ecu_manifest_signed
+
       # TODO: For now, the ASN.1 data keeps all the ECU Manifests in a list
       # rather than having a dictionary with index ECU Serial and value list of
       # ECU Manifests for that ECU Serial. This should probably be changed,
@@ -49,7 +63,7 @@ def get_asn_signed(json_signed):
       # exterior labeling of ECU Serial and the contents of the ECU Manifest
       # (which could hypothetically incorrectly contain the wrong ECU Serial?
       # That is checked by the Primary, but... this still bears thought.)
-      ecuVersionManifests[numberOfECUVersionManifests] = ecuVersionManifest
+      ecuVersionManifests[numberOfECUVersionManifests] = temp_ecu_manifest
       numberOfECUVersionManifests += 1
 
   signed['numberOfECUVersionManifests'] = numberOfECUVersionManifests
@@ -69,11 +83,35 @@ def get_json_signed(asn_metadata):
   json_manifests = {}
   numberOfECUVersionManifests = int(asn_signed['numberOfECUVersionManifests'])
   ecuVersionManifests = asn_signed['ecuVersionManifests']
+
+  # TODO: Don't simply assume that the number of manifests here is actually
+  # equal to numberOfECUVersionManifests. We don't really care in Python about
+  # this number.... Make sure to generate a meaningful exception if you can't
+  # just iterate. Consider raising a warning if the number doesn't
+  # match the actual number of ECU Manifests in here. (Would still be Primary-
+  # validated via signature.)
   for i in range(numberOfECUVersionManifests):
+
     manifest = ecuVersionManifests[i]
-    json_manifest = ecu_manifest_asn1_coder.asn_to_json_metadata(
-        ecu_manifest_asn1_coder.get_json_signed, manifest)
-    json_manifests.append(json_manifest)
+
+    json_manifest_signed = ecu_manifest_asn1_coder.get_json_signed(
+        manifest) # Weird: this takes the full manifest, not just 'signed'
+
+    ecu_serial = json_manifest_signed['ecu_serial']
+
+    import uptane.encoding.asn1_codec as asn1_codec # TODO: <~> Fix circular import and move.
+    json_manifest_sigs = asn1_codec.convert_signatures_to_json(
+        manifest['signatures'])
+
+    json_manifest = {
+        'signatures': json_manifest_sigs, 'signed': json_manifest_signed}
+
+    # TODO: I think it's fine, but consider repercussions of trusting this ECU
+    # Serial inside the manifest here.
+    if ecu_serial not in json_manifests:
+      json_manifests[ecu_serial] = []
+    json_manifests[ecu_serial].append(json_manifest)
+
   json_signed['ecu_version_manifests'] = json_manifests
 
   return json_signed
